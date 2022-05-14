dda77380bfc872882b6b555fb915977c8c4cfa12
Alex <alexchurilov@mail.ru>
2022-05-14 15:46:39 +0300

    Add Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа принимает на вход цифру обозначающую день недели и проверяет, является ли он выходным
+
+Console.WriteLine("Введите число от 1 до 7:");
+int dayNumber = int.Parse(Console.ReadLine());
+if (dayNumber == 6 || dayNumber == 7)
+    Console.WriteLine("да");
+else if (dayNumber < 1 || dayNumber > 7)
+    Console.WriteLine("Введенное число не соответствует условию");
+else
+    Console.WriteLine("нет");
