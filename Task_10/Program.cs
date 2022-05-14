c5625dddc1b7403c6346d3fd1f1f25024b398012
Alex <alexchurilov@mail.ru>
2022-05-14 15:08:01 +0300

    Create Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа принимет на вход трёхзначное число и показывает 2ю цифру этого числа
+
+Console.WriteLine("Введите трёхзначное число");
+int number = int.Parse(Console.ReadLine());
+int secondDigit = (number / 10) % 10;
+Console.WriteLine(secondDigit);
+
